097608d8a8257a608c9631163900e26a70c5110c
Aleksandar Bozic <bozapro@gmail.com>
2016-03-08 01:23:40 +0100

    Added sample project setup.

---
+++ sample/src/main/java/com/bozapro/sample/MainActivity.java
@@ -0,0 +1,32 @@
+package com.bozapro.sample;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.bozapro.perorez.AndroidVersion;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Snackbar.make(view, "This device is running Android KitKat or newer:" + AndroidVersion.isKitKatOrHigher(), Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
